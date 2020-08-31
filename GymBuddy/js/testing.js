3ccb0ce98dcb116f3528f7ae7893e39d4dba9ff1
MorneDT <160249@virtualwindow.co.za>
2020-08-31 18:50:00 +0200

    Reps and Sets Now lock when checked

--- GymBuddy/js/testing.js
+++ GymBuddy/js/testing.js
@@ -5,71 +5,71 @@ let id = 0;
 
 $(document).ready(function () {
 
-  /*DELETE*/
-  $('body').on('click', '.fa-trash', function () {
+  // /*DELETE*/
+  // $('body').on('click', '.fa-trash', function () {
 
-    if ($('.exerciseMain ul li').hasClass("temp_deleted")) {
-      $('.exerciseMain ul li.temp_deleted').remove();
-    }
+  //   if ($('.exerciseMain ul li').hasClass("temp_deleted")) {
+  //     $('.exerciseMain ul li.temp_deleted').remove();
+  //   }
 
-    list_item_to_undo = $(this).parent().parent();
-    var undo_time = 10000;
+  //   list_item_to_undo = $(this).parent().parent();
+  //   var undo_time = 10000;
 
-    list_item_to_undo.animate({
-      height: "0px"
-    }, 200, function () {
-      $(this).addClass('temp_deleted').hide();
-    });
+  //   list_item_to_undo.animate({
+  //     height: "0px"
+  //   }, 200, function () {
+  //     $(this).addClass('temp_deleted').hide();
+  //   });
 
-    function_undo(list_item_to_undo, undo);
+  //   function_undo(list_item_to_undo, undo);
 
-    //undo
-    $('.undo').addClass('active');
+  //   //undo
+  //   $('.undo').addClass('active');
 
-    clearTimeout(time_out);
+  //   clearTimeout(time_out);
 
-    time_out = setTimeout(function () {
-      $('.undo').removeClass('active');
-      if (undo === false) {
-        $('.exerciseMain ul li.temp_deleted').remove();
-      }
-    }, undo_time);
+  //   time_out = setTimeout(function () {
+  //     $('.undo').removeClass('active');
+  //     if (undo === false) {
+  //       $('.exerciseMain ul li.temp_deleted').remove();
+  //     }
+  //   }, undo_time);
 
-  });
-  /*DELETE*/
-
-
-  /*UNDO*/
-  $('.undo div').click(function () {
-    undo = true;
-    function_undo(list_item_to_undo, undo);
-    $(this).parent().removeClass('active');
-  });
+  // });
+  // /*DELETE*/
 
 
-  /*EDIT*/
-  $('body').on('click', '.fa-pencil', function () {
-    var current = $(this).parent().parent().find('span').text();
-    $(this).parent().parent().find('input[type=text]').val(current).show().select();
-  });
+  // /*UNDO*/
+  // $('.undo div').click(function () {
+  //   undo = true;
+  //   function_undo(list_item_to_undo, undo);
+  //   $(this).parent().removeClass('active');
+  // });
 
 
-  $('body').on('keypress', '.exerciseMain ul li input[type=text]', function (e) {
-    if (e.which == 13) {
-      //hide undo
-      $('.undo').removeClass('active');
-
-      var newvalue = $(this).val();
-      $(this).parent().parent().find('label span.item-name').text(newvalue);
-      $(this).hide();
-      return false;    //<---- Add this line
-    }
-  });
+  // /*EDIT*/
+  // $('body').on('click', '.fa-pencil', function () {
+  //   var current = $(this).parent().parent().find('span').text();
+  //   $(this).parent().parent().find('input[type=text]').val(current).show().select();
+  // });
 
 
-  $(document).on('blur', 'input[type=text]', function () {
-    $(this).hide();
-  });
+  // $('body').on('keypress', '.exerciseMain ul li input[type=text]', function (e) {
+  //   if (e.which == 13) {
+  //     //hide undo
+  //     $('.undo').removeClass('active');
+
+  //     var newvalue = $(this).val();
+  //     $(this).parent().parent().find('label span.item-name').text(newvalue);
+  //     $(this).hide();
+  //     return false;    //<---- Add this line
+  //   }
+  // });
+
+
+  // $(document).on('blur', 'input[type=text]', function () {
+  //   $(this).hide();
+  // });
 
 
 
@@ -140,7 +140,20 @@ function function_undo(list_item_to_undo, undo) {
     list_item_to_undo.removeClass('temp_deleted');
   }
 }
-
+function readOnlyFunction(id){
+var element = document.getElementById('label_'+ id);
+console.log(element);
+console.log(document.getElementById(id).checked);
+if(document.getElementById(id).checked == true){
+  element.querySelector("#sets_"+ id).setAttribute("readonly", true);
+  element.querySelector("#reps_"+ id).setAttribute("readonly", true);
+  // element.elements["#sets_"+ id].setAttribute("readonly", true);
+  // element.elements["#reps_"+ id].setAttribute("readonly", true);
+}else{
+  element.querySelector("#sets_"+ id).removeAttribute("readonly");
+  element.querySelector("#reps_"+ id).removeAttribute("readonly");
+}
+}
 
 const sortable = document.getElementById("sortable");
 function addTodo(toDo, id) {
@@ -151,19 +164,19 @@ function addTodo(toDo, id) {
            <circle cy="7" cx="14" r="1" fill="#999"></circle><circle cy="12.5" cx="9.5" r="1" fill="#999"></circle>
            <circle cy="12.5" cx="14" r="1" fill="#999"></circle><circle cy="18" cx="9.5" r="1" fill="#999"></circle><circle cy="18" cx="14" r="1" fill="#999"></circle></svg>
          </div>
-         <input type="checkbox" id="${id}" />
-         <label for="${id}" >
+         <input type="checkbox" id="${id}" onchange="readOnlyFunction(${id})" />
+         <label id="label_${id}" for="${id}" >
            <div class="checkbox"><span class="fa fa-check"></span></div>
            <span class="item-name">${toDo} </span>
            <input type="text">
-           <label1 class = "RepsSets" for ="sets">Sets</label1>
-           <input type="number" id="sets" min="1" max="999" placeholder="--"> 
-           <label1 class = "RepsSets" for = "reps">Reps</label1>
-            <input type="number" id="reps" min="1" max="999" placeholder="--">
+           <label1 class = "RepsSets" for ="sets_${id}">Sets</label1>
+           <input type="number" id="sets_${id}" min="1" max="999" placeholder="--"> 
+           <label1 class = "RepsSets" for = "reps_${id}">Reps</label1>
+            <input type="number" id="reps_${id}" min="1" max="999" placeholder="--">
          </label>
          <div class="actions">
            <!--<span class="fa fa-pencil"></span>-->
-           <span class="fa fa-trash"></span>
+           <!--<span class="fa fa-trash"></span>-->
          </div>
        </li>
   `;
@@ -174,3 +187,5 @@ function addTodo(toDo, id) {
 
 addTodo("Push-ups", id++);
 addTodo("Wide Push-ups", id++);
+addTodo("Diamond Push-ups", id++);
+
